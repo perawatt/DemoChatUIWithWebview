9e490b0a9ca034b780a8a73af2f72a46d64e1890
perawatt <pekung4433@perfenterprise.com>
2020-12-03 10:05:13 +0700

    init

---
+++ DemoChatUIWithWebview/DemoChatUIWithWebview/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DemoChatUIWithWebview
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            chatInput.UnFocusEntry();
+        }
+    }
+}
---
+++ DemoChatUIWithWebview/DemoChatUIWithWebview/Views/Partials/ChatInputBarView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DemoChatUIWithWebview.Views.Partials
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ChatInputBarView : ContentView
+    {
+        public ChatInputBarView()
+        {
+            InitializeComponent();
+
+            if (Device.RuntimePlatform == Device.iOS)
+            {
+                this.SetBinding(HeightRequestProperty, new Binding("Height", BindingMode.OneWay, null, null, null, chatTextInput));
+            }
+        }
+
+        public void UnFocusEntry()
+        {
+            chatTextInput?.Unfocus();
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            chatTextInput.Text = string.Empty;
+            chatTextInput.Focus();
+        }
+    }
+}
